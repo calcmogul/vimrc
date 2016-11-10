dbf8854bfb7075303380e2f719af094126b841de
Tyler Veness <calcmogul@gmail.com>
2016-11-09 16:27:11 -0800

    Removed unnecessary argument from setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 # $1 is absolute path to vimrc Git repository
 
 if [ $# -ne 1 ]; then
-  echo Usage: ./setup.sh ~/vimrc
+  echo Usage: ./setup.sh
   exit 1
 fi
 
 mkdir -p ~/.config
-ln -s $1/nvim ~/.config/nvim
+ln -s $PWD/nvim ~/.config/nvim
 ln -s ~/.config/nvim ~/.vim
 ln -s ~/.config/nvim/init.vim ~/.vimrc
